3d580a25a01e0c447d6b30b77b0d9c34cfa9550f
hannamakarychak <annamakarichak3@gmail.com>
2021-08-08 17:49:27 +0200

    add local storage

--- src/components/reviews-list/reviews-list.jsx
+++ src/components/reviews-list/reviews-list.jsx
@@ -1,20 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../button/button";
 import Modal from "../modal/modal";
 import Review from "../review/review";
 
 import "./reviews-list.scss";
 
+const REVIEWS_STORAGE_KEY = "reviews";
+
 const ReviewsList = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const handleOpenModal = () => setIsModalOpen(true);
   const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const initialReviews = localStorage.getItem(REVIEWS_STORAGE_KEY);
+    setReviews(JSON.parse(initialReviews));
+  }, []);
 
   const handleCloseModal = (newReview) => {
     const newReviews = reviews.slice();
     newReviews.push(newReview);
 
     setReviews(newReviews);
+    localStorage.setItem(REVIEWS_STORAGE_KEY, JSON.stringify(newReviews));
     setIsModalOpen(false);
   };
 
