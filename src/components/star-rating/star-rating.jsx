e8b2158651847fffdb52efab52dd13cd8c01729f
hannamakarychak <annamakarichak3@gmail.com>
2021-08-13 16:12:12 +0200

    refactor star rating to use npm package

--- src/components/star-rating/star-rating.jsx
+++ src/components/star-rating/star-rating.jsx
@@ -1,49 +1,20 @@
 import classNames from "classnames";
-import { Fragment } from "react";
-import "./star-rating.scss";
+import ReactStars from "react-rating-stars-component";
 
-const STARS = [
-  {
-    rating: 5,
-    label: "Идеально",
-  },
-  {
-    rating: 4,
-    label: "Отлично",
-  },
-  {
-    rating: 3,
-    label: "Хорошо",
-  },
-  {
-    rating: 2,
-    label: "Так себе",
-  },
-  {
-    rating: 1,
-    label: "Ужасно",
-  },
-];
+import "./star-rating.scss";
 
 const StarRating = ({ onChange, className = "" }) => {
   return (
     <div className={classNames("star-rating", className)}>
       <p className="star-rating__caption">Оцените товар:</p>
-      <div className="star-rating__group">
-        {STARS.map(({ rating, label }) => (
-          <Fragment key={`rating-${rating}`}>
-            <input
-              className="star-rating__input"
-              type="radio"
-              name="rating"
-              id={`rating-${rating}`}
-              value={rating}
-              onClick={() => onChange(rating)}
-            />
-            <label className="star-rating__star" htmlFor={`rating-${rating}`} aria-label={label} />
-          </Fragment>
-        ))}
-      </div>
+      <ReactStars
+        count={5}
+        onChange={onChange}
+        isHalf={false}
+        emptyIcon={<span className="star-rating__star star-rating__star--empty" />}
+        filledIcon={<span className="star-rating__star star-rating__star--full" />}
+        activeColor="#ffd700"
+      />
     </div>
   );
 };
